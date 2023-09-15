b65d2a8ab60648a4fdc668672e8778e4186d2b27
Brandon <102290201+icyde@users.noreply.github.com>
2023-09-15 10:33:04 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const iframe = document.createElement("iframe");
+iframe.style.position = "fixed";
+iframe.style.zIndex = "999"
+iframe.style.right = "10px"
+iframe.style.bottom = "50px"
+iframe.style.width = "1000px"
+iframe.style.height = "1000px"
+iframe.src = "https://chat-hq8nbcnvb-icyde.vercel.app/";
+document.body.appendChild(iframe);
