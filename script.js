17daa455acb1f4e28ce78cc759b99dbc553b2af0
Brandon <102290201+icyde@users.noreply.github.com>
2023-09-15 22:22:50 +0800

    fix: add onclick test...

--- script.js
+++ script.js
@@ -1,45 +1,122 @@
-const iframe = document.createElement("iframe");
-iframe.style.position = "fixed";
-iframe.setAttribute("id", "chatbot-iframe")
-iframe.style.zIndex = "999"
-iframe.style.right = "10px"
-iframe.style.bottom = "50px"
-iframe.style.width = "100px"
-iframe.style.height = "100px"
-//iframe.style.border = "none"
-iframe.src = "https://chat-bot-fawn-one.vercel.app/";
-iframe.onload = function () {
-  let showModal = false;
-  const button = document.getElementById("chatbot-button");
-  button.addEventListener("click", () => {
-    if (showModal) {
-      iframe.style.width = "100px";
-      iframe.style.height = "100px";
-    } else {
-      iframe.style.width = "415px";
-      iframe.style.height = "700px";
-    }
-  });
-};
-document.body.appendChild(iframe);
+// const iframe = document.createElement("iframe");
+// iframe.style.position = "fixed";
+// iframe.setAttribute("id", "chatbot-iframe")
+// iframe.style.zIndex = "999"
+// iframe.style.right = "10px"
+// iframe.style.bottom = "50px"
+// iframe.style.width = "100px"
+// iframe.style.height = "100px"
+// //iframe.style.border = "none"
+// iframe.src = "https://chat-bot-fawn-one.vercel.app/";
+// document.body.appendChild(iframe);
 
-// iframe width:415px h:700px
-// const handleLoad = () =>{
-//   let showModal = false;
-//   const button = document.getElementById("chatbot-button");
-//   const iframe2 = document.getElementById("chatbot-iframe");
-//   button.addEventListener("click", () => {
-//     if (showModal) {
-//       iframe2.style.width = "100px";
-//       iframe2.style.height = "100px";
-//     } else {
-//       iframe2.style.width = "415px";
-//       iframe2.style.height = "700px";
-//     }
-//   });
-// }
-// iframe.addEventListener("load", handleLoad, true);
+// window.addEventListener("message", (evt) => {
+//   if (evt.origin !== "https://chat-bot-fawn-one.vercel.app") {
+//     return;
+//   }
 
+//   if (evt.data === "show") {
+//     api.hide();
+//   }
+// });
+
+(() => {
+  const script = document.currentScript;
+
+  const loadWidget = () => {
+    const widget = document.createElement("div");
+
+    const widgetStyle = widget.style;
+    widgetStyle.display = "none";
+    widgetStyle.boxSizing = "border-box";
+    widgetStyle.width = "100px";
+    widgetStyle.height = "100px";
+    widgetStyle.position = "fixed";
+    widgetStyle.bottom = "50px";
+    widgetStyle.right = "10px";
+
+    const iframe = document.createElement("iframe");
+
+    const iframeStyle = iframe.style;
+    iframeStyle.boxSizing = "borderBox";
+    iframeStyle.position = "fixed";
+    iframeStyle.right = 0;
+    iframeStyle.bottom = 0;
+    iframeStyle.width = "100%";
+    iframeStyle.height = "100%";
+    iframeStyle.border = 0;
+    iframeStyle.margin = 0;
+    iframeStyle.padding = 0;
+    iframeStyle.width = "100px";
+
+    widget.appendChild(iframe);
+
+    const greeting = script.getAttribute("data-greeting");
+
+    const api = {
+      sendMessage: (message) => {
+        iframe.contentWindow.postMessage(
+          {
+            sendMessage: message,
+          },
+          "https://chat-bot-fawn-one.vercel.app"
+        );
+      },
+
+      show: () => {
+        widget.style.height= "700px";
+        widget.style.width = "415px"
+      },
+
+      hide: () => {
+        widget.style.height = "100px";
+        widget.style.width = "100px";
+      },
+
+      toggle: () => {
+        const display = window.getComputedStyle(widget, null).display;
+        widget.style.display = display === "none" ? "block" : "none";
+      },
+    };
+
+    iframe.addEventListener("load", () => {
+      window.addEventListener("getWidgetApi", () => {
+        const event = new CustomEvent("widgetApi", { detail: api });
+        window.dispatchEvent(event);
+      });
+
+      window.addEventListener("message", (evt) => {
+        if (evt.origin !== "https://chat-bot-fawn-one.vercel.app") {
+          return;
+        }
+
+        if (evt.data === "hide") {
+          api.hide();
+        }
+
+        if (evt.data === "show"){
+          api.show();
+        }
+      });
+
+      widgetStyle.display = "block";
+    });
+
+    iframe.src = "https://chat-bot-fawn-one.vercel.app";
+
+    document.body.appendChild(widget);
+  };
+
+  if (document.readyState === "complete") {
+    loadWidget();
+  } else {
+    document.addEventListener("readystatechange", () => {
+      if (document.readyState === "complete") {
+        loadWidget();
+      }
+    });
+  }
+})();
 
 
 
