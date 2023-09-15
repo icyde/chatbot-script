af10cfd26a5ae6b53be5670edaea85ae2f83d8c6
Brandon <102290201+icyde@users.noreply.github.com>
2023-09-15 17:25:03 +0800

    fix: check if iframe is fit content

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ iframe.setAttribute("id", "chatbot-iframe")
 iframe.style.zIndex = "999"
 iframe.style.right = "10px"
 iframe.style.bottom = "50px"
-iframe.style.width = "100%"
-iframe.style.height = "fit-content"
+iframe.style.width = "650px"
+iframe.style.height = "405px"
 //iframe.style.border = "none"
 iframe.src = "https://chat-bot-fawn-one.vercel.app/";
 document.body.appendChild(iframe);
