db51943efd07f12b53c31e3acc28b30a799a0d78
Brandon <102290201+icyde@users.noreply.github.com>
2023-09-15 17:43:19 +0800

    fix: add onclick test

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ iframe.setAttribute("id", "chatbot-iframe")
 iframe.style.zIndex = "999"
 iframe.style.right = "10px"
 iframe.style.bottom = "50px"
-iframe.style.width = "650px"
-iframe.style.height = "405px"
+iframe.style.width = "100px"
+iframe.style.height = "100px"
 //iframe.style.border = "none"
 iframe.src = "https://chat-bot-fawn-one.vercel.app/";
 document.body.appendChild(iframe);
 
+// iframe width:415px h:700px
+let showModal = false
+const button = document.getElementById("chatbot-button");
+button.addEventListener("click", ()=>{
+  if (showModal){
+    iframe.style.width = "100px"
+    iframe.style.height = "100px"
+  }
+  else {
+    iframe.style.width = "415px"
+    iframe.style.height = "700px"
+  }
+
+})
